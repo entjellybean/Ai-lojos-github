7b522254c5a54053e9984c1751d6b0aa4008bc0d
Laget-Thomas <zoe.lagetthomas@gmail.com>
2024-12-02 13:37:26 +0100

    Update ColorSensor.java
    modifs

--- src/perception/ColorSensor.java
+++ src/perception/ColorSensor.java
@@ -11,34 +11,36 @@ import lejos.robotics.SampleProvider;
 import lejos.utility.Delay;
 
 public class ColorSensor {
-   EV3ColorSensor colorSensor;
+	EV3ColorSensor colorSensor;
 
-   EV3LargeRegulatedMotor leftMotor;
-   EV3LargeRegulatedMotor rightMotor;
 
-   public static void main(String[] args) {
-       new ColorSensor();
-   }
+	public static void main(String[] args) {
+		new ColorSensor();
+	}
 
-   public ColorSensor() {}
-	
-   // Function to differentiate between gray or white
-   private boolean detecteLigneBlanche() {
-       SampleProvider lightProvider = colorSensor.getRedMode();
-       float[] sample = new float[lightProvider.sampleSize()];
-       lightProvider.fetchSample(sample, 0);
-       float lightValue = sample[0] * 100; // Scale to percentage
-   boolean b = false;
-	   for(int i=0; i<2;i++) {
-    	   System.out.println("Light Value: " + lightValue);
-	   }
-      
-       if (lightValue > 40 && lightValue < 80) {
-    	   b = true; //si la couleur est blanche
-       } 
-       return b;
-   }
-public void close() {
+	public ColorSensor() {
+		Port s4 = LocalEV3.get().getPort("S4");
+		colorSensor = new EV3ColorSensor(s4);
+	}
+
+	// Function to differentiate between gray or white
+	public boolean detecteLigneBlanche() {
+		SampleProvider lightProvider = colorSensor.getRedMode();
+		float[] sample = new float[lightProvider.sampleSize()];
+		lightProvider.fetchSample(sample, 0);
+		float lightValue = sample[0] * 100; // Scale to percentage
+		boolean b = false;
+		for(int i=0; i<2;i++) {
+			System.out.println("Light Value: " + lightValue);
+		}
+
+		if (lightValue > 70) {
+			b = true; //si la couleur est blanche
+		} 
+		return b;
+	}
+
+	public void close() {
 		colorSensor.close();
 	}
 }
