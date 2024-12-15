6d744a1792f6bff623d3f4ec646782de777d6ec2
Yass4real <yassmina.cherqaoui@gmail.com>
2024-12-15 12:32:06 +0100

    Ajoute des commentaires javadoc pour la classe ColorSensor

--- src/perception/ColorSensor.java
+++ src/perception/ColorSensor.java
@@ -10,19 +10,36 @@ import lejos.robotics.Color;
 import lejos.robotics.SampleProvider;
 import lejos.utility.Delay;
 
+/**
+ * La classe ColorSensor permet de gérer un capteur de couleur EV3. 
+ * Elle est utilisée pour détecter des surfaces blanches en analysant 
+ * les valeurs mesurées par le capteur.
+ * @author Basak Unal
+ */
 public class ColorSensor {
-	EV3ColorSensor colorSensor;
-
+	EV3ColorSensor colorSensor; // Capteur de couleur EV3
 
 	public static void main(String[] args) {
 		//new ColorSensor();
 	}
 
+	/**
+        * Constructeur de la classe ColorSensor.
+        * Initialise le capteur de couleur connecté à un port donné.
+        *
+        * @param s4 Port auquel le capteur de couleur EV3 est connecté.
+        */
 	public ColorSensor(Port s4) {
 		colorSensor = new EV3ColorSensor(s4);
 	}
 
-	// Function to differentiate between gray or white
+	/**
+        * Vérifie si une surface blanche est détectée.
+        * La méthode utilise le mode "Red" du capteur pour mesurer la lumière réfléchie
+        * et considère la surface blanche si les valeurs sont entre 40% et 80%.
+        *
+        * @return true si une surface blanche est détectée, false sinon.
+        */
 	public boolean detecteLigneBlanche() {
 		SampleProvider lightProvider = colorSensor.getRedMode();
 		float[] sample = new float[lightProvider.sampleSize()];
@@ -39,6 +56,11 @@ public class ColorSensor {
 		return b;
 	}
 
+	/**
+        * Ferme et termine l'utilisation du capteur de couleur.
+        * Cette méthode doit être appelée pour libérer les ressources matérielles associées
+        * au capteur une fois son utilisation terminée.
+        */
 	public void close() {
 		colorSensor.close();
 	}
