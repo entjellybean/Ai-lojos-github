d5093f183a249f9ad3b1b47932023fbc4412ccc1
Yass4real <yassmina.cherqaoui@gmail.com>
2024-12-15 13:33:09 +0100

    Ajout commentaires JavaDoc pour la Classe MainClass

--- src/agent/MainClass.java
+++ src/agent/MainClass.java
@@ -19,16 +19,34 @@ import perception.BoutonConsole;
 import perception.ColorSensor;
 import perception.MoveScan;
 
-public class MainClass {
-	private Deplacement mouvementRobot;
-	private Pince pince;
-	private ColorSensor colorSensor;
-	private BoutonConsole boutonConsole;
-	private MoveScan moveScan;
-	private boolean isFirstPalletPicked = false;
-	private boolean isSecondPalletPicked = false;
-	private int positionRobotEnFace;
 
+/**
+ * La classe MainClass implémente la stratégie du robot pour la compétition contre un robot adversaire.
+ * 
+ * <p>La stratégie suit plusieurs étapes :</p>
+ * <ul>
+ *   <li>Récupération des deux premiers palets selon des trajectoires prédéfinies.</li>
+ *   <li>Recherche et manipulation des palets restants.</li>
+ *   <li>Gestion dynamique des obstacles, comme les murs et le robot adverse.</li>
+ *   <li>Retour à une position centrale après chaque dépôt pour optimiser les mouvements.</li>
+ * </ul>
+ * @author Développement : Basak Unal, Zoe Laget Thomas
+ * @author Stratégie : Basak Unal, Narta Neziraj, Yassmina Cherqaoui, Zoe Laget Thomas
+ */
+public class MainClass {
+	private Deplacement mouvementRobot; // Gère les déplacements du robot
+	private Pince pince;  // Manipule les palets
+	private ColorSensor colorSensor; // Détecte les lignes blanches
+	private BoutonConsole boutonConsole;  // Gère les interactions utilisateur via les boutons
+	private MoveScan moveScan; // Permet de scanner l'environnement pour détecter les palets
+	private boolean isFirstPalletPicked = false; // Indique si le premier palet a été récupéré
+	private boolean isSecondPalletPicked = false; // Indique si le deuxième palet a été récupéré
+	private int positionRobotEnFace; / Position initiale du robot adverse (donnée par nous via le bouton)
+
+	/**
+        * Constructeur de la classe MainClass.
+        * Configure tout le nécessaire pour exécuter la stratégie.
+        */
 	public MainClass() {
 		Port A = LocalEV3.get().getPort("A");
 		Port B = LocalEV3.get().getPort("B");
@@ -48,20 +66,37 @@ public class MainClass {
 
 	}
 
+	/**
+        * Point d'entrée du programme.
+        * Lance la stratégie de compétition pour récupérer et déposer les palets 
+        * en suivant les étapes définies dans la méthode {@link #start()}.
+        */
 	public static void main(String[] args) {
 		MainClass robot = new MainClass();
 		robot.start();
 	}
 
+	
+        /**
+        * Exécute la stratégie principale du robot.
+        * <p>Les étapes incluent :</p>
+        * <ul>
+        *   <li>Récupération et dépôt des deux premiers palets à des positions fixes.</li>
+        *   <li>Recherche dynamique des palets restants.</li>
+        *   <li>Évitement des obstacles (mur ou robot adverse).</li>
+        * </ul>
+        */
 	public void start() {
-		pince.fermer(1000);
+		pince.fermer(1000); // Ferme la pince pour initialiser la position
 		System.out.println("wait...");
+		 // L'utilisateur indique la position du robot adverse via le bouton
 		positionRobotEnFace = boutonConsole.positionRobot();
 
+		// Boucle principale de la stratégie
 		while (Button.ESCAPE.isUp()) {
 
 			if (!isFirstPalletPicked) {
-				//Programme spécifique pour récupérer le premier palet avec les distances
+				// Récupération et dépôt du premier palet
 				mouvementRobot.avancerDe(45);
 				pince.lacherPalet();
 				mouvementRobot.avancerDe(10);
@@ -73,13 +108,13 @@ public class MainClass {
 				isFirstPalletPicked = true;
 
 			} else if(!isSecondPalletPicked) {
-				//Programme spécifique pour récupérer le deuxième palet avec les distances et angle
+				// Récupération et dépôt du deuxième palet
 				goToSecondPalletAndPick();
 				isSecondPalletPicked = true;
 				mouvementRobot.pivoterGauche(163);
 				mouvementRobot.avancerDe(100);
 			} else{
-				//effectue une recherche pour identifier le prochain palet, le récupérer et le déposer derrière la ligne blanche
+				 // Recherche et collecte des palets restants
 				searchAndCollectNextPallet();
 			}
 		}
@@ -89,15 +124,12 @@ public class MainClass {
 		cleanup();
 	}
 
-
+	/**
+        * Déplace le robot vers la ligne blanche et dépose le premier palet.
+        * Cette méthode utilise une trajectoire prédéfinie sans gestion d'obstacles.
+        */
 	private void goToWhiteLineAndDrop1erPalet() {
-		//une fois le palet récupéré, 
-		//cette méthode permet d'aller le déposer à la ligne blanche ne contient
-		//pas de méthode pour éviter l'autre robot car la stratégie n'en a pas besoin
-		//pour les deux premiers palets.
-
-		//avance jusqu'à la ligne blanche
-		mouvementRobot.avancerContinu(true);
+		mouvementRobot.avancerContinu(true); //avance jusqu'à la ligne blanche
 		boolean res = false;
 		System.out.println("Is moving : "+mouvementRobot.isMoving());
 		while (mouvementRobot.isMoving() && !res) {
@@ -117,11 +149,14 @@ public class MainClass {
 
 	}
 
+	/**
+        * Une fois le palet récupéré, cette méthode permet d'aller le déposer à la ligne blanche.
+        * 
+        * <p>Elle contient une méthode pour éviter l'autre robot, car on utilise cette méthode 
+        * pour la partie non déterministe.</p>
+        */
 	private void goToWhiteLineAndDrop() { 
-		//une fois le palet récupéré, 
-		//cette méthode permet d'aller le déposer à la ligne blanche contient une
-		// méthode pour éviter l'autre robot car on est utilise cette méthode pour la
-		// partie non déterministe
+		
 
 		mouvementRobot.avancerContinu(true);
 		boolean res = false;
@@ -134,36 +169,36 @@ public class MainClass {
 			}
 		}
 		mouvementRobot.stop();
-		pince.lacherPalet();    
-		// Reculer après avoir déposé le palet
-		mouvementRobot.reculerDe(10); // Recule de 20 cm (ajustez la distance si nécessaire)
+		pince.lacherPalet();   //deposer le palet 
+		mouvementRobot.reculerDe(10); // Recule de 10 cm (ajuster la distance si nécessaire)
 		System.out.println("Recul après dépôt.");
-
-		// Fermer la pince après le recul
-		pince.fermer(1000);
+		pince.fermer(1000); // Ferme la pince après le recul
 		System.out.println("Pince fermée.");
 
 	}
-
+	
+	/**
+        * La méthode a pour objectif de récupérer le 2ème palet et de le récupérer.
+        * 
+        * <p>Comme notre robot avait tendance à se déplacer sur la gauche, 
+        * les angles ne sont pas symétriques pour compenser.</p>
+        * 
+        * <p>positionRobotEnFace est récupéré lors du démarrage du robot en appuyant 
+        * sur le bouton correspondant par l'utilisateur en fonction de la position 
+        * du robot en face.</p>
+        * 
+        * <p>s'oriente, va vers le robot et récupère le palet.</p>
+        */
 	private void goToSecondPalletAndPick() {
-		// La méthode a pour objectif de récupérer le 2ème palet et de le récupérer
 		
-		//Comme notre robot avait tendance à se déplacer sur la gauche, 
-		//les angles ne sont pas symétriques pour compenser
-		
-		// positionRobotEnFace est récupéré lors du démarage du robot en appuyant sur le
-		// bouton correspondant par l'utilisateur en fonction de la position du robot en face
-		
-		//s'oriente, va vers le robot et récupère le palet
-		if (positionRobotEnFace == 0) {
-			//si le robot est à gauche
+		if (positionRobotEnFace == 0) { // Robot adverse à gauche
 			mouvementRobot.pivoterGauche(150);
 			mouvementRobot.avancerDe(25);
 			pince.lacherPalet();
 			mouvementRobot.avancerDe(23);
 			pince.recupererPalet();
 
-		} else if (positionRobotEnFace == 1) {
+		} else if (positionRobotEnFace == 1) { // Robot adverse au centre
 			//si le robot est au milieu
 			mouvementRobot.pivoterDroite(130);
 			mouvementRobot.avancerDe(35);
@@ -172,8 +207,7 @@ public class MainClass {
 			pince.recupererPalet();
 
 			mouvementRobot.setOrientation(130);
-		} else if (positionRobotEnFace == 2) {
-			//si le robot est à droite
+		} else if (positionRobotEnFace == 2) { // Robot adverse a droite
 			mouvementRobot.pivoterGauche(150);
 			mouvementRobot.avancerDe(25);
 			pince.lacherPalet();
@@ -181,35 +215,32 @@ public class MainClass {
 			pince.recupererPalet();
 		}
 		
-		//s'oriente et va à la ligne blanche
-		if (positionRobotEnFace == 0) {
-			//si le robot est à gauche
+		if (positionRobotEnFace == 0) { //si le robot est à gauche
 			mouvementRobot.orienterVersLigneAdverse();
 			mouvementRobot.setOrientation(0);
 
-		} else if (positionRobotEnFace == 1) {
-			//si le robot est au milieu
+		} else if (positionRobotEnFace == 1) { //si le robot est au milieu
 			mouvementRobot.orienterVersLigneAdverse();
 			mouvementRobot.setOrientation(0);
-		} else if (positionRobotEnFace == 2) {
-			//si le robot est à droite
+		} else if (positionRobotEnFace == 2) { //si le robot est à droite
 			mouvementRobot.orienterVersLigneAdverse();
 			mouvementRobot.setOrientation(0);
 		}
-		
+		 // Orientation vers la ligne blanche
 		goToWhiteLineAndDrop1erPalet();
 		mouvementRobot.stop();
 	}
 
+	/**
+	* Départ de la méthode au milieu de la table, effectue une recherche
+	* Si la recherche est concluante (la 1ere ou 2ème discontinuité est un palet,
+	* on va le récupérer, le déposé derrière la ligne blanche et retourné au milieu)
+	* Sinon, au sein de la classe RechercheEtOrientation, on va lui demander de tourner
+	* de 90° et d'avancer de 50 cm, le while du start permettre de relancer une nouvelle
+	* recherche
+	*/
 	private void searchAndCollectNextPallet() {
-		//Départ de la méthode au milieu de la table, effectue une recherche
-		//Si la recherche est concluante (la 1ere ou 2ème discontinuité est un palet,
-		//on va le récupérer, le déposé derrière la ligne blanche et retourné au milieu)
-		//Sinon, au sein de la classe RechercheEtOrientation, on va lui demander de tourner
-		//de 90° et d'avancer de 50 cm, le while du start permettre de relancer une nouvelle
-		//recherche
 		
-
 		if(moveScan.RechercheEtOrientation()) {
 			System.out.println("Il y a un palet");
 			pince.lacherPalet();
@@ -223,19 +254,27 @@ public class MainClass {
 		}
 	}
 
-
+	/**
+	* Si le capteur détect quelque chose en dessous de 25 cm, ça ne peut pas être un palet
+	* Il faut donc l'éviter
+ 	*  @return true si un obstacle est détecté à moins de 25 cm, false sinon.
+	*/
 	private boolean detectObstacle() {
-		//Si le capteur détect quelque chose en dessous de 25 cm, ça ne peut pas être un palet
-		//Il faut donc l'éviter
 		if (moveScan.getDistance()>=25){
 			return false;
 		}
 		return true;
 	}
 
+	/**
+        * Évite un obstacle détecté en ajustant la trajectoire du robot.
+        * <p>Gère deux cas :</p>
+        * <ul>
+        *   <li>Obstacle de type mur : le robot tourne à 90 degrés pour contourner.</li>
+        *   <li>Obstacle de type robot : le robot ajuste sa position pour éviter la collision.</li>
+        * </ul>
+        */
 	private void eviterObstacle() {
-		//Si on a détecté un obstacle, on pourra utiliser cette classe pour l'éviter
-		// on va traiter les deux cas où c'est un mur ou un robot
 		
 		double distanceCentre = moveScan.getDistance();
 		mouvementRobot.pivoterGauche(30);
@@ -252,8 +291,7 @@ public class MainClass {
 			//Si c'est vrai, c'est un mur, on va l'éviter à 90 degrés
 			mouvementRobot.pivoterDroite(90);
 		} else {
-			//si c'est un robot, on va l'éviter en avançant après avoir pris de l'angle, puis
-			//en retournant à notre orientation de base
+			 // C'est un robot : ajuster la trajectoire
 			boolean b = true;
 			int cpt=30;
 			mouvementRobot.pivoterDroite(30);
@@ -272,6 +310,9 @@ public class MainClass {
 
 	}
 
+	 /**
+         * Arrête tous les composants du robot.
+         */
 	private void cleanup() {
 		mouvementRobot.stop();
 		moveScan.close();
